b73522582cc290ce9fb30f37c76894ab484edb37
jonghun <jonghuncu@gmail.com>
2023-02-07 21:22:53 +0900

    feat : 웹 계층 개발 및 README.md 파일 작성

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +15,29 @@ import java.util.List;
 public class ItemService {
     private final ItemRepository itemRepository;
 
+    /**
+     *  // 스프링 트랜잭션어노테이션에 의해 커밋이 이루어짐
+     *  커밋이 되면 JPA는 flush를 날림 - 변경된 애를 다 찾음 - set으로 인해 바뀐것들
+     *  이게 감지되면 SQL 쿼리를 날림
+     *  따라서 따로 itemRepository.save(findItem); 처럼 저장을 하지 않아도 됨
+     */
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
     }
 
-    public List<Item> findItem() {
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+        findItem.change(name, price, stockQuantity);
+
+
+    }
+
+    public List<Item> findItems() {
         return itemRepository.findAll();
     }
 
