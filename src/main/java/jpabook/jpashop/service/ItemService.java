931066e0ed6373322a714b3c50a84fdea2b398b9
jonghun <jonghuncu@gmail.com>
2023-03-11 13:26:29 +0900

    feat : service update

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -16,7 +16,7 @@ public class ItemService {
     private final ItemRepository itemRepository;
 
     /**
-     *  // 스프링 트랜잭션어노테이션에 의해 커밋이 이루어짐
+     *  스프링 트랜잭션어노테이션에 의해 커밋이 이루어짐
      *  커밋이 되면 JPA는 flush를 날림 - 변경된 애를 다 찾음 - set으로 인해 바뀐것들
      *  이게 감지되면 SQL 쿼리를 날림
      *  따라서 따로 itemRepository.save(findItem); 처럼 저장을 하지 않아도 됨
@@ -32,9 +32,6 @@ public class ItemService {
         findItem.setPrice(price);
         findItem.setName(name);
         findItem.setStockQuantity(stockQuantity);
-        findItem.change(name, price, stockQuantity);
-
-
     }
 
     public List<Item> findItems() {
